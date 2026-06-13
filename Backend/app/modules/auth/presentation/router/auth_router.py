4e9c0db937da1af5833a2863e636b488b2ea4955
BrayanReyes213 <brayanreyesog@gmail.com>
2026-06-12 21:02:17 -0500

    correction

--- Backend/app/modules/auth/presentation/router/auth_router.py
+++ Backend/app/modules/auth/presentation/router/auth_router.py
@@ -1,4 +1,19 @@
-@router.post("/register")
+from fastapi import APIRouter, Depends
+
+from app.modules.auth.presentation.schemas.register_response import RegisterResponse
+from app.modules.auth.application.use_cases.register_user import RegisterUserUseCase
+from app.modules.auth.presentation.schemas.register_request import RegisterRequest
+
+router = APIRouter(
+    prefix="/auth",
+    tags=["Authentication"]
+)
+
+
+@router.post(
+    "/register",
+    responseModel=RegisterResponse
+)
 def register(
     request: RegisterRequest,
     use_case: RegisterUserUseCase = Depends()
