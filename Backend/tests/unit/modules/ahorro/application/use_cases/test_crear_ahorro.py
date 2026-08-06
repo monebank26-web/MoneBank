1c308db31c2e9e4a7df5a7696aa6812144437d89
BrayanReyes213 <brayanreyesog@gmail.com>
2026-08-06 14:39:06 -0500

    pruebas unitarias brayan

---
+++ Backend/tests/unit/modules/ahorro/application/use_cases/test_crear_ahorro.py
@@ -0,0 +1,36 @@
+from unittest.mock import Mock
+
+from app.modules.ahorro.application.use_cases.crear_ahorro import CrearAhorro
+
+
+def test_debe_crear_un_ahorro():
+
+    # Arrange
+    repository = Mock()
+    db = Mock()
+
+    ahorro_data = {
+        "nombre": "Viaje",
+        "meta": 500000
+    }
+
+    ahorro_creado = {
+        "id": 1,
+        "nombre": "Viaje",
+        "meta": 500000
+    }
+
+    repository.create.return_value = ahorro_creado
+
+    use_case = CrearAhorro(repository)
+
+    # Act
+    resultado = use_case.execute(db, ahorro_data)
+
+    # Assert
+    repository.create.assert_called_once_with(
+        db,
+        ahorro_data
+    )
+
+    assert resultado == ahorro_creado
--- Backend/tests/unit/modules/cuenta/application/use_cases/test_obtener_cuenta_por_id.py
+++ Backend/tests/unit/modules/cuenta/application/use_cases/test_obtener_cuenta_por_id.py
@@ -4,7 +4,6 @@ from app.modules.cuenta.application.use_cases.obtener_cuenta_por_id import (
     ObtenerCuentaPorIdUseCase
 )
 
-
 def test_debe_obtener_cuenta_por_id():
 
     repository = Mock()
@@ -23,5 +22,5 @@ def test_debe_obtener_cuenta_por_id():
 
     repository.get_by_id.assert_called_once_with(db, 1)
 
-    assert resultado == cuenta
-
+    assert resultado["success"] is True
+    assert resultado["cuenta"] == cuenta
